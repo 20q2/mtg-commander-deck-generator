093455fd3cd6f16887a8e8132cd9b93a9b545cbf
20q2 <andrewnintendo@msn.com>
2026-06-13 15:03:24 -0400

    feat(brew): engine barrel + full-loop convergence test

---
+++ src/services/brew/engine.ts
@@ -0,0 +1,6 @@
+export * from './brewTypes';
+export { buildHealth, isComplete, NONLAND_COMPLETE_RATIO } from './health';
+export { buildScoringContext, scoreCandidate } from './scoring';
+export { applyPick, undoLast, type ApplyPickMeta } from './picks';
+export { nextRoutes } from './routes';
+export { openNode, deriveReasons } from './nodes';
